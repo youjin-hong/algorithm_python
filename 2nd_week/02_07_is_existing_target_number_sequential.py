b181d784d47fabfb2ff0e9140896b3bfe9a355ac
youjin-hong <asdfghjk1052@naver.com>
2026-02-12 18:05:45 +0900

    순차 탐색 함수명 정정

--- 2nd_week/02_07_is_existing_target_number_sequential.py
+++ 2nd_week/02_07_is_existing_target_number_sequential.py
@@ -3,12 +3,13 @@ finding_target = 14
 finding_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 
-def is_existing_target_number_binary(target, array):
-    for num in array:
-        if num == target:
+def is_existing_target_number_sequential(target, array):
+    for number in array:
+        if target == number:
             return True
+
     return False
 
 
-result = is_existing_target_number_binary(finding_target, finding_numbers)
-print(result)
+result = is_existing_target_number_sequential(finding_target, finding_numbers)
+print(result)  # True
