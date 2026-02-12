6789550cf30048297e457a1cdabf80c9cf61851f
youjin-hong <asdfghjk1052@naver.com>
2026-02-12 18:04:40 +0900

    순차 탐색

---
+++ 2nd_week/02_07_is_existing_target_number_sequential.py
@@ -0,0 +1,14 @@
+# 순차 탐색 먼저 살펴보기. 그 다음에 이진 탐색 알아볼 것임
+finding_target = 14
+finding_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+
+def is_existing_target_number_binary(target, array):
+    for num in array:
+        if num == target:
+            return True
+    return False
+
+
+result = is_existing_target_number_binary(finding_target, finding_numbers)
+print(result)
