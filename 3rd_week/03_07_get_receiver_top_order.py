ee38bd12fa067e27d3cb2f67fa4587b6027a88a4
youjin-hong <asdfghjk1052@naver.com>
2026-02-25 17:34:42 +0900

    반복문을 이용한 탑 문제

---
+++ 3rd_week/03_07_get_receiver_top_order.py
@@ -0,0 +1,21 @@
+top_heights = [6, 9, 5, 7, 4]
+
+# stack이 아닌 반복문을 이용한 풀이
+
+def get_receiver_top_orders(heights):
+    answer = [0] * len(heights)
+
+    for i in range(len(heights) - 1, 0, -1):
+        for j in range(i - 1, -1, -1):
+            if heights[i] <= heights[j]:
+                answer[i] = j + 1
+                break
+
+    return answer
+
+
+print(get_receiver_top_orders(top_heights))  # [0, 0, 2, 2, 4] 가 반환되어야 한다!
+
+print("정답 = [0, 0, 2, 2, 4] / 현재 풀이 값 = ",get_receiver_top_orders([6,9,5,7,4]))
+print("정답 = [0, 0, 2, 3, 3, 3, 6] / 현재 풀이 값 = ",get_receiver_top_orders([3,9,9,3,5,7,2]))
+print("정답 = [0, 0, 2, 0, 0, 5, 6] / 현재 풀이 값 = ",get_receiver_top_orders([1,5,3,6,7,6,5]))
