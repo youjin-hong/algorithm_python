9554dcb2ebd2f78c4849102dc316166f73fd32f0
youjin-hong <asdfghjk1052@naver.com>
2026-02-25 16:04:06 +0900

    선택 정렬

---
+++ 3rd_week/03_02_selection_sort.py
@@ -0,0 +1,22 @@
+input = [4, 6, 2, 9, 1]
+
+# 선택 정렬도 시간 복잡도 O(N^2)
+def selection_sort(array):
+    n = len(array)
+
+    for i in range(n - 1):
+        min_index = i
+        for j in range(n - i):
+            if array[i + j] < array[min_index]:
+                min_index = i + j
+        array[i], array[min_index] = array[min_index], array[i]
+
+    return array
+
+
+selection_sort(input)
+print(input) # [1, 2, 4, 6, 9] 가 되어야 합니다!
+
+print("정답 = [1, 2, 4, 6, 9] / 현재 풀이 값 = ",selection_sort([4, 6, 2, 9, 1]))
+print("정답 = [-1, 3, 9, 17] / 현재 풀이 값 = ",selection_sort([3,-1,17,9]))
+print("정답 = [-3, 32, 44, 56, 100] / 현재 풀이 값 = ",selection_sort([100,56,-3,32,44]))
