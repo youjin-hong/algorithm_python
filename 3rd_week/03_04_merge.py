9e92e572f8e0c605fe96cb61934395b6603fd3ce
youjin-hong <asdfghjk1052@naver.com>
2026-02-25 16:52:09 +0900

    병합정렬

---
+++ 3rd_week/03_04_merge.py
@@ -0,0 +1,37 @@
+array_a = [1, 2, 3, 5]
+array_b = [4, 6, 7, 8]
+
+# 얘는 시간 복잡도 O(
+
+# 코딩 테스트에서 직접 구현해보라고 자주 나오는 문제임
+# 그만큼 복잡하고 좀 어려움
+def merge(array1, array2):
+    result = []
+
+    array1_index = 0
+    array2_index = 0
+
+    while array1_index < len(array1) and array2_index < len(array2):
+        if array1[array1_index] < array2[array2_index]:
+            result.append(array1[array1_index])
+            array1_index += 1
+        elif array1[array1_index] > array2[array2_index]:
+            result.append(array2[array2_index])
+            array2_index += 1
+
+    while array1_index < len(array1):
+        result.append(array1[array1_index])
+        array1_index += 1
+
+    while array2_index < len(array2):
+        result.append(array2[array2_index])
+        array2_index += 1
+
+    return result
+
+
+print(merge(array_a, array_b))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
+
+print("정답 = [-7, -1, 5, 6, 9, 10, 11, 40] / 현재 풀이 값 = ", merge([-7, -1, 9, 40], [5, 6, 10, 11]))
+print("정답 = [-1, 2, 3, 5, 10, 40, 78, 100] / 현재 풀이 값 = ", merge([-1,2,3,5,40], [10,78,100]))
+print("정답 = [-1, -1, 0, 1, 6, 9, 10] / 현재 풀이 값 = ", merge([-1,-1,0], [1, 6, 9, 10]))
---
+++ 3rd_week/03_05_merge_sort.py
@@ -0,0 +1,48 @@
+array = [5, 3, 2, 1, 6, 8, 7, 4]
+
+# 총 O(NlogN)
+
+# 재귀 함수는 항상 탈출 조건이 있어야 함
+# 얘는 시간 복잡도 O(logN)
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+
+    left_array = array[:mid]
+    right_array = array[mid:]
+    return merge(merge_sort(left_array), merge_sort(right_array))
+
+
+# 얘는 시간복잡도 O(N)
+def merge(array1, array2):
+    result = []
+    array1_index = 0
+    array2_index = 0
+
+    while array1_index < len(array1) and array2_index < len(array2):
+        if array1[array1_index] < array2[array2_index]:
+            result.append(array1[array1_index])
+            array1_index += 1
+        elif array1[array1_index] > array2[array2_index]:
+            result.append(array2[array2_index])
+            array2_index += 1
+
+    if array1_index == len(array1):
+        while array2_index < len(array2):
+            result.append(array2[array2_index])
+            array2_index += 1
+
+    if array2_index == len(array2):
+        while array1_index < len(array1):
+            result.append(array1[array1_index])
+            array1_index += 1
+
+    return result
+
+print(merge_sort(array))  # [1, 2, 3, 4, 5, 6, 7, 8] 가 되어야 합니다!
+
+print("정답 = [-7, -1, 5, 6, 9, 10, 11, 40] / 현재 풀이 값 = ", merge_sort([-7, -1, 9, 40, 5, 6, 10, 11]))
+print("정답 = [-1, 2, 3, 5, 10, 40, 78, 100] / 현재 풀이 값 = ", merge_sort([-1, 2, 3, 5, 40, 10, 78, 100]))
+print("정답 = [-1, -1, 0, 1, 6, 9, 10] / 현재 풀이 값 = ", merge_sort([-1, -1, 0, 1, 6, 9, 10]))
