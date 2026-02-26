7643b1397edc1199a0af1345722f4ec77905524c
youjin-hong <asdfghjk1052@naver.com>
2026-02-26 17:27:42 +0900

    재귀를 이용한 dfs 구현(추천x)

---
+++ 4th_week/04_03_dfs_recursion.py
@@ -0,0 +1,32 @@
+# 위의 그래프를 예시로 삼아서 인접 리스트 방식으로 표현했습니다!
+graph = {
+    1: [2, 5, 9],
+    2: [1, 3],
+    3: [2, 4],
+    4: [3],
+    5: [1, 6, 8],
+    6: [5, 7],
+    7: [6],
+    8: [5],
+    9: [1, 10],
+    10: [9]
+}
+visited = []
+
+# 1. 시작 노드인 1부터 탐색한다.
+# 2. 현재 방문한 노드를 visited_array에 추가한다.
+# 3. 현재 방문한 노드와 인접한 노드 중 방문하지 않은 노드를 방문한다.
+def dfs_recursion(adjacent_graph, cur_node, visited_array):
+    visited.append(cur_node)
+
+    for adjacent_node in adjacent_graph[cur_node]:
+        if adjacent_node not in visited_array: # 방문하지 않았다면
+            dfs_recursion(adjacent_graph, adjacent_node, visited_array)
+            # 깊이가 수백만이다 하면 재귀함수를 이용해 dfs를 쓰는 것은 옳지 않다. recursion 에러를 맞닥뜨릴 수 있음
+
+
+    return
+
+
+dfs_recursion(graph, 1, visited)  # 1 이 시작노드입니다!
+print(visited)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 이 출력되어야 합니다!
