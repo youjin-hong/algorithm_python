588d6883472661f694dfddac7beb5a938f05799e
youjin-hong <asdfghjk1052@naver.com>
2026-02-27 17:09:44 +0900

    가장 큰 수 문자열 더 쉽게 만들기

--- programmers/sort/가장큰수.py
+++ programmers/sort/가장큰수.py
@@ -7,3 +7,11 @@ def solution(numbers):
     str_arr.sort(key=lambda item: item * 3, reverse=True)
 
     return str(int(''.join((str_arr))))
+
+# 문자열 더 쉽게 만들기
+def solution(numbers):
+    str_arr = list(map(str, numbers))
+
+    str_arr.sort(key=lambda item: item * 3, reverse=True)
+
+    return str(int(''.join((str_arr))))
