98190703aa4ea67c6afd98f5abc0a7cf0ad33ef9
youjin-hong <asdfghjk1052@naver.com>
2026-03-23 19:57:22 +0900

    폰켓몬

--- programmers/hash/폰켓몬.py
+++ programmers/hash/폰켓몬.py
@@ -13,6 +13,16 @@ def solution(nums):
     return min(len(dict), possible_count)
 
 
+# 두 번째 내 풀이
+def solution(nums):
+    hash_dict = dict()
+
+    for num in nums:
+        h = hash(num)
+        hash_dict[h] = num
+
+    return min(len(hash_dict), len(nums) // 2)
+
 # 효율적인 풀이
 def solution(ls):
     return min(len(ls)/2, len(set(ls)))
