938de101d5cbd1c29ed19c8e4c749825c5fe2985
youjin-hong <asdfghjk1052@naver.com>
2026-03-23 20:03:01 +0900

    전화번호 목록

--- programmers/hash/전화번호목록.py
+++ programmers/hash/전화번호목록.py
@@ -7,3 +7,21 @@ def solution(phone_book):
             return False
 
     return True
+
+# 해시를 이용한 풀이
+def solution(phone_book):
+    # 1. 모든 전화번호를 해시 맵(딕셔너리)에 담습니다. 📥
+    hash_map = {}
+    for number in phone_book:
+        hash_map[number] = True
+
+    # 2. 각 전화번호의 접두어가 해시 맵에 있는지 확인합니다. 🔍
+    for number in phone_book:
+        prefix = ""
+        for digit in number:
+            prefix += digit
+            # 현재까지 만든 접두어가 해시 맵에 있고, 본인이 아니라면?
+            if prefix in hash_map and prefix != number:
+                return False
+
+    return True
