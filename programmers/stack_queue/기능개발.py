53706609dfeebbb1f3788bab403acf8cf5eac586
youjin-hong <asdfghjk1052@naver.com>
2026-03-27 19:28:19 +0900

    기능 개발

--- programmers/stack_queue/기능개발.py
+++ programmers/stack_queue/기능개발.py
@@ -21,3 +21,29 @@ def solution(progresses, speeds):
         result.append(count)
 
     return result
+
+# 두 번째 내 풀이
+import math
+
+
+def solution(progresses, speeds):
+    launch_days = []
+
+    for i in range(len(progresses)):
+        complete_progress = math.ceil((100 - progresses[i]) / speeds[i])
+        launch_days.append(complete_progress)
+
+    result = []
+    count = 1
+    max_day = launch_days[0]
+    for i in range(1, len(launch_days)):
+
+        if max_day >= launch_days[i]:
+            count += 1
+        else:
+            max_day = launch_days[i]
+            result.append(count)
+            count = 1
+    result.append(count)
+
+    return result
