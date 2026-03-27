ac12b9667a84d86b64f506fbe7e614454cfabe88
youjin-hong <asdfghjk1052@naver.com>
2026-03-27 21:21:12 +0900

    주식 가격

--- programmers/stack_queue/주식가격.py
+++ programmers/stack_queue/주식가격.py
@@ -12,3 +12,18 @@ def solution(prices):
         stack.append(time)
 
     return stack
+
+# 두 번째 풀이(스택 활용 x)
+def solution(prices):
+    result = []
+
+    for i in range(len(prices)):
+        seconds = 0
+        for j in range(i + 1, len(prices)):
+            seconds += 1
+            if prices[i] > prices[j]:
+                break
+        result.append(seconds)
+
+    return result
+
