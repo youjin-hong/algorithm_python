7b18efa536ac443484ff44f3ce8103534162d5d9
youjin-hong <asdfghjk1052@naver.com>
2026-03-27 19:34:40 +0900

    올바른 괄호

--- programmers/stack_queue/올바른괄호.py
+++ programmers/stack_queue/올바른괄호.py
@@ -14,3 +14,17 @@ def solution(s):
             stack.pop()
 
     return len(stack) == 0
+
+# 두 번째 내 풀이
+def solution(s):
+    if s[0] != '(' or s[-1] != ')': return False
+
+    stack = []
+
+    for ch in s:
+        if len(stack) == 0 or ch == "(":
+            stack.append(ch)
+        else:
+            stack.pop()
+
+    return len(stack) == 0
